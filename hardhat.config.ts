1fa2337235e05d3038277a1b1f6071ddde3d7694
Bernard <benbaah104@gmail.com>
2025-03-15 21:22:59 +0000

    hardhat configuration

---
+++ hardhat.config.ts
@@ -0,0 +1,18 @@
+// hardhat.config.ts
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.28",
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_URL,
+      accounts: [process.env?.TESTNET_PRIVATE_KEY as string]
+    }
+  }
+};
+
+export default config;
